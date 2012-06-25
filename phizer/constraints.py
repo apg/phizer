5239d0ec8b09de1f67ad64d0e5e36a11b50453a1
Ariel Elbaz <arielbaz@gmail.com>
2012-06-25 16:03:04 -0400

    remove most of phizer cropping. max = scale down if relevant. square = crop max square and scale down if relevant. portrait = may crop sides of a wide image. landscape = may crop top&bottom of a tall image.

--- phizer/constraints.py
+++ phizer/constraints.py
@@ -4,57 +4,64 @@ import logging
 
 def constrain_square(fw, fh, tw, th):
     """Generates steps to make a square out of the source and target
-    dimensions.
+    dimensions. 
 
-    Ensures that steps always produce a square, but the square may be
-    smaller than the target dimensions depending on the source size
+    1. crop to a square
+    2. if target is smaller than source, shrink it
     """
     steps = []
     
-    # if it's a square, then we're going to be square regardless.
-    tw = max(tw, th)
-    th = tw
+    # crop to square
+    extra_width = fw - fh
+    crop = abs(extra_width / 2)
+    if extra_width > 0: # landscape
+        steps.append(('crop', (crop, 0, fw - crop, fh)))
+    elif extra_width < 0: # portrait
+        steps.append(('crop', (0, crop, fw, fh - crop)))
 
-    if tw > fw or th > fh:
-        # one of the dimensions isn't large enough, so we'll scale it
-        tw = min(fw, fh)
-        th = tw
+    # target should always be square
+    target_size = max(tw,th)
+    
+    # scale never scales up (only down), so this would work ok even if we unconditionly append 'scale'.
+    if target_size < fh or target_size < fw:
+        steps.append(('scale', (target_size, target_size)))
+    return steps
 
-    # crop to square and scale to (tw, th)
-    diff = fw - fh
-    if diff > 0: # landscape
-        extra = diff / 2
-        steps.append(('crop', (extra, 0, fw - extra, fh)))
-    elif diff < 0: # portrait
-        extra = abs(diff / 2)
-        steps.append(('crop', (0, extra, fw, fh - extra)))
-
-    steps.append(('scale', (tw, th)))
+def constrain_max(fw, fh, tw, th):
+    """Generates steps to scale down the image, as long as it fits within tw x th rectangle.
+    """
+    
+    logging.debug("at constrain max: %dx%d => %dx%d" % (fw,fh,tw,th))
+    steps = [('scale', (tw, th))]
     return steps
 
 def constrain_portrait(fw, fh, tw, th):
     """Generates steps to make a portrait out of the source and target
     dimensions.
+    May crop sides of a wide image, will never crop top or bottom.
 
-    Ensures that steps always produce a portrait, but the portrait may
-    be smaller than the target dimensions depending on the source size
+    1. If image is taller than target, scale down
+    2. If image is too wide, crop sides
     """
-    if tw > fw or th > fh:
-        return _crop_and_keep(fw, fh, tw, th)
+
+    if fw <= tw and fh <= th:
+        return []
 
     steps = []
+    scale_ratio = float(th) / fh
+    if scale_ratio > 1.0:
+        # no need to scale down
+        inter_height = fh
+        inter_width = fw
+    else:
+        inter_height = th
+        inter_width = int(fw * scale_ratio)
+        steps.append(('scale', (inter_width, inter_height)))
 
-    ratio_h = th / float(fh)
-    inter_height = th
-    inter_width = int(ratio_h * fw)
-
-    steps.append(('scale', (inter_width, inter_height)))
-    
-    if inter_width > tw:
-        tw = int(tw * ratio_h)
-        wdiff = inter_width - tw
-        steps.append(('crop', (abs(wdiff/2), 0, 
-                               inter_width - abs(wdiff/2), inter_height)))
+    crop_width = inter_width - tw
+    if crop_width > 0:
+        delta = int(crop_width / 2)
+        steps.append(('crop', (delta, 0, inter_width - delta, inter_height)))
 
     return steps
 
@@ -62,59 +69,30 @@ def constrain_portrait(fw, fh, tw, th):
 def constrain_landscape(fw, fh, tw, th):
     """Generates steps to make a landscape out of the source and target
     dimensions.
+    May crop top&bottom of a tall image, will never crop sides.
 
-    Ensures that steps always produce a landscape, but the portrait may
-    be smaller than the target dimensions depending on the source size
+    1. If image is wider than target, scale down
+    2. If image is too high, crop top and bottom
     """
-    if tw > fw or th > fh:
-        return _crop_and_keep(fw, fh, tw, th)
+
+    if fw <= tw and fh <= th:
+        return []
 
     steps = []
+    scale_ratio = float(tw)/fw
+    if scale_ratio > 1.0:
+        # no need to scale down
+        inter_height = fh
+        inter_width = fw
+    else:
+        inter_width = tw
+        inter_height = int(fh * scale_ratio)
+        steps.append(('scale', (inter_width, inter_height)))
 
-    ratio_w = tw / float(fw)
-    inter_width = tw
-    inter_height = int(ratio_w * fh)
-
-    steps.append(('scale', (inter_width, inter_height)))
-
-    if inter_height > th:
-        th = int(th * ratio_w)
-        hdiff = inter_height - th
-        steps.append(('crop', (0, abs(hdiff/2), 
-                               inter_width, inter_height - abs(hdiff/2))))
+    crop_height = inter_height - th
+    if crop_height > 0:
+        delta = int(crop_height / 2)
+        steps.append(('crop', (0, delta, inter_width, inter_height - delta)))
 
     return steps
 
-def constrain_max(fw, fh, tw, th):
-    if fw == fh:
-        return constrain_square(fw, fh, tw, th)
-    frls = (fw - fh) > 0    
-    if frls:
-        return constrain_landscape(fw, fh, tw, th)
-    else:
-        return constrain_portrait(fw, fh, tw, th)
-
-
-def _crop_and_keep(fw, fh, tw, th):
-    wd = fw - tw
-    hd = fh - th
-
-    tx = 0
-    ty = 0
-    bx = fw
-    by = fh
-
-#    import pdb
-#    pdb.set_trace()
-
-    if wd > 0:
-        # crop horizontally
-        tx += (wd / 2) 
-        bx -= (wd / 2)
-
-    if hd > 0:
-        # crop vertically
-        ty += (hd / 2) 
-        by -= (hd / 2)
-
-    return [('crop', (tx, ty, bx, by))]
